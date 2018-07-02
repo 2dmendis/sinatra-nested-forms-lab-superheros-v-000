82a8bc0f799a68df70d390b123cb1167c0564cf4
Dmitri Mendis <2dmendis@gmail.com>
2018-07-02 18:02:16 +0000

    automatically backed up by learn

---
+++ app/models/hero.rb
@@ -0,0 +1,10 @@
+class Hero 
+  attr_accessor :name, :power, :bio 
+  @@all = []
+  
+  def initialize(details)
+    @name = details[:name]
+    @power = details[:power]
+    @bio = details[:bio]
+  end 
+end 
